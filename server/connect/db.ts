8ff0b817b174ee28a67c2acbf04590a5d8d249dd
Rikie Patrick <rikiepatrick@gmail.com>
2024-04-16 12:21:20 +0100

    update colors to work with ts

--- server/connect/db.ts
+++ server/connect/db.ts
@@ -3,8 +3,8 @@ import colors from 'colors';
 
 const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI);
-		console.log(`Database connected ${conn.connection.host}`.cyan.underline);
+		const conn = await mongoose.connect(process.env.MONGO_URI!);
+		console.log(colors.cyan.underline(`Database connected ${conn.connection.host}`));
 	} catch (error) {
 		console.log(error);
 		process.exit(1);
